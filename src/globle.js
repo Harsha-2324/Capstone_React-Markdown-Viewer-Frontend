70273cd521310aca7d18bf22fee3816b824bd110
Harsha-2324 <rharshva@gmail.com>
2024-05-16 22:00:30 +0530

    Capstone Commit

--- src/globle.js
+++ src/globle.js
@@ -1,2 +1,2 @@
 // export const API = "http://localhost:4000"
- export const API = "https://capstone-react-markdown-viewer-backend-mcxe.onrender.com/" 
+ export const API = "https://capstone-react-markdown-viewer-backend.vercel.app/"; 
